48f7f200f7406bd8da206af43bb387f05d302972
Arun37-Kumar <arun007624@gmail.com>
2022-04-09 16:06:47 +0530

    UC3 updated

--- Generics/MaximumNumberCheck.cs
+++ Generics/MaximumNumberCheck.cs
@@ -55,5 +55,29 @@ namespace Generics
             }
             return 0;
         }
+
+        //Find Max String
+        public static string MaximumString(string firstValue, string secondValue, string thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return null;
+        }
     }
 }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -18,6 +18,10 @@ namespace Generics
             float findMaxFloat = MaximumNumberCheck.MaximumFloatNumbers(12.12f,23.23f,43.23f);
             Console.WriteLine("Them maximum float number is : "+findMaxFloat);
 
+            //String Maximum Find
+            string findMaxString = MaximumNumberCheck.MaximumString("Apple","Peach","Banana");
+            Console.WriteLine("The maximum string value is : "+findMaxString);
+
             Console.ReadLine();
         }
     }
